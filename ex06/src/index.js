ff2269013fca61208d64c584b056b0903c2a2fa6
Mirela <adisserdarevic@gmail.com>
2021-03-30 21:33:18 +0200

    Added folder ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+// change code below this line
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+ReactDOM.render(<App />, document.getElementById('root'));
+// change code above this line
