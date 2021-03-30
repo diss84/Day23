6c625d60491d675140f0b4e38384365ac9593709
Mirela <adisserdarevic@gmail.com>
2021-03-30 18:37:10 +0200

    Added folder ex02

---
+++ ex02/src/Plant.js
@@ -0,0 +1,16 @@
+//Plant.js
+import React, { Component } from 'react';
+class Plant extends Component {
+    constructor(props) {
+    super(props);
+    this.state = { type: "Flowering plant" }
+    }
+    render() {
+        return (
+            <div>
+                <h2>Plant type is {this.state.type}!</h2>
+            </div>
+        );
+    }
+}
+export default Plant;
